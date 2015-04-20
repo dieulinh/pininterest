c776109a45e8837c11ff3c6932646d9c567c8cf4
Linh Nguyen <linhvn09@gmail.com>
2015-04-20 02:01:05 -0400

    upload image adding

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -11,11 +11,11 @@ class PinsController < ApplicationController
     redirect_to root_path
   end
 	def new
-		@pin = Pin.new
+		@pin = current_user.pins.build
 	end
 
 	def create
-		@pin = Pin.new(pin_params)
+		@pin = current_user.pins.build(pin_params)
 		if @pin.save
 			redirect_to @pin, notice: "Successfully Create New Pin"
 		else
@@ -39,7 +39,7 @@ class PinsController < ApplicationController
 	private
 
 	def pin_params
-		params.require(:pin).permit(:title, :description)
+		params.require(:pin).permit(:title, :description, :image)
 	end
 
 	def find_pin
